a5991547950a15b8073fb133e26eb6bc5a6051b3
Rupali Anap <anaprupali333@gmail.com>
2023-01-18 02:31:10 -0800

    Added: Inserted At Index

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -2,35 +2,51 @@ package com.bridgelabz;
 
 public class LinkedList<T> {
 
-        Node<T> head;
-        public void add(T value) {
-            Node<T> newNode = new Node<T>();
-            newNode.data = value;
-            newNode.next = null;
-            if (head == null) {
-                head = newNode;
-            } else {
-                Node<T> last = head;
-                while (last.next != null) {
-                    last = last.next;
-                }
-                last.next = newNode;
+    Node<T> head;
+
+    public void add(T value) {
+        Node<T> newNode = new Node<T>();
+        newNode.data = value;
+        newNode.next = null;
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> last = head;
+            while (last.next != null) {
+                last = last.next;
             }
+            last.next = newNode;
         }
-        public void show() {
+    }
+
+    public void show() {
+        Node<T> node = head;
+        while (node.next != null) {
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+
+    public void addAtStart(T value) {
+        Node<T> newNode = new Node<>();
+        newNode.data = value;
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addAtIndex(int index, T data) {
+        Node<T> newNode = new Node<>();
+        newNode.data = data;
+        if (index == 0) {
+            addAtStart(data);
+        } else {
             Node<T> node = head;
-            while (node.next != null) {
-                System.out.println(node.data);
+            for (int i = 0; i < index - 1; i++) {
                 node = node.next;
             }
-            System.out.println(node.data);
+            newNode.next = node.next;
+            node.next = newNode;
         }
-
-        public void addAtStart(T value) {
-            Node<T> newNode = new Node<>();
-            newNode.data = value;
-            newNode.next = head;
-            head = newNode;
-        }
-
+    }
 }
