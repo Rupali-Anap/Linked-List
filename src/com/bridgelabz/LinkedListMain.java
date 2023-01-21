7cf1895ed4e0ef1104c01aaf2a8422d617afc358
Rupali Anap <anaprupali333@gmail.com>
2023-01-21 01:19:37 -0800

    Added: Insert elements in List

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -8,8 +8,10 @@ public class LinkedListMain {
         LinkedList1.add(70);
         LinkedList1.addAtIndex(1, 30);
         LinkedList1.show();
-        String searchresult = (LinkedList1.searchByValue(30) == -1) ? "value not found in the list" : "value found at index " + LinkedList1.searchByValue(30);
-        System.out.println(searchresult);
+        String searchResult = (LinkedList1.searchByValue(30) == -1) ? "value not found in the list" : "value found at index " + LinkedList1.searchByValue(30);
+        System.out.println(searchResult);
+        System.out.println("After Inserting");
+        LinkedList1.searchAndInsert(30, 40);
+        LinkedList1.show();
     }
-
 }
